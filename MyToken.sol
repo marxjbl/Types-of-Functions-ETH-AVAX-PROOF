32fe8dc53fd3384f6327f012f8f5cc3a97b9238a
Marx <marxj.lizardo@gmail.com>
2024-08-04 10:24:45 +0800

    MyToken.sol

--- MyToken.sol
+++ MyToken.sol
@@ -7,16 +7,20 @@ import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.5.0/contr
 contract MyToken is ERC20, Ownable {
     constructor() ERC20("My Token", "MTK") {
         // Initialize the contract with the token name and symbol
-        _mint(msg.sender, 1000000 * 10**decimals()); // Mint 1 million tokens to the contract owner
     }
 
-    // Only allow the owner to mint new tokens
-    function mint(address to, uint256 amount) public onlyOwner {
-        _mint(to, amount);
+    // Override the _mint function to only allow the owner to mint tokens
+    function _mint(address account, uint256 amount) internal override onlyOwner {
+        super._mint(account, amount);
     }
 
     // Only allow the owner to burn tokens
     function burn(uint256 amount) public onlyOwner {
         _burn(msg.sender, amount);
+    }
+
+    // Function to mint the initial tokens
+    function mintInitialTokens() public onlyOwner {
+        _mint(msg.sender, 1000000 * 10**decimals());
     }
 }
